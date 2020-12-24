c10e2aced08c16a865372736be14372aa0cb3c1e
SAlexW <38391381+SAlexW@users.noreply.github.com>
2020-12-24 19:45:10 +1000

    Update Installer.lua

--- Installer.lua
+++ Installer.lua
@@ -15,8 +15,6 @@ local webconnect = component.isAvailable("internet")
 if webconnect then wwweb = require("internet") else io.stderr:write("No internet connection. Please install an\nInternet Card\n") os.exit(false) end
 local type = false
 
-local url = "https://raw.githubusercontent.com/SAlexW/AunisUniversalDialer/main"
-
 function dircreate()
  if not fsys.isDirectory("/arud") then
   local sccs, msg = fsys.makeDirectory("/arud")
@@ -42,85 +40,26 @@ end
 function download()
  print("Downloading files...")
  if type then
-  
+ shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalDialer/main/Tablet.lua /arud/AUD.lua -fQ")
+ shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalDialer/main/MWAS.ff /arud/MWAS.ff -fQ")
+ shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalDialer/main/MWDS.ff /arud/MWDS.ff -fQ")
+ shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalDialer/main/MWGS.ff /arud/MWGS.ff -fQ")
+ shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalDialer/main/MWG.ff /arud/MWG.ff -fQ")
+ shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalDialer/main/UNG.ff /arud/UNG.ff -fQ")
+ shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalDialer/main/GI.ff /arud/GI.ff -fQ")
  else
-    local file = 
+ shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalDialer/main/PC.lua /arud/AUD.lua -fQ") 
  end
+local autorunFile = [[
+local shell = require("shell")
+
+do
+shell.setWorkingDirectory("/arud/")
+shell.execute("AUD.lua")
 end
-
---[[
-
-https://computercraft.ru/topic/2110-zagruzka-programm-s-github/
-
-local args, opts = shell.parse(...)
-
-
-
-function downloadNeededFiles()
-  print("Downloading Files, Please Wait...")
-  downloadFile(ReleaseVersionsFile)
-  local file = io.open(ReleaseVersionsFile)
-  ReleaseVersions = serialization.unserialize(file:read("*a"))
-  file:close()
-  downloadManifestedFiles(ReleaseVersions.launcher)
-  if opts.d then ReleaseVersions.launcher.dev = true end
-  file = io.open("/ags/installedVersions.ff", "w")
-  file:write("{launcher="..serialization.serialize(ReleaseVersions.launcher)..",}")
-  file:close()
+]]
+local file = io.open("/autorun.lua", "w")
+file:write(autorunFile)
+file:close()
+print("Download completed. Please, restart your PC/Tablet. Programm will run automatically")
 end
-
-function createSystemShortcut()
-  local agsBinFile = [[
-shell = require("shell")
-filesystem = require("filesystem")
-
-local args, opts = shell.parse(...)
-
-if filesystem.exists("/ags/AuspexGateSystems.lua") then
-  local options = "-"
-  for k,v in pairs(opts) do options = options..tostring(k) end
-  shell.execute("/ags/AuspexGateSystems.lua "..options)
-else
-  io.stderr:write("Auspex Gate Systems is Not Correctly Installed\n")
-end
-  ]]
-  local file = io.open("/bin/ags.lua", "w")
-  file:write(agsBinFile)
-  file:close()
-end
-
-function downloadManifestedFiles(program)
-  for i,v in ipairs(program.manifest) do downloadFile(v) end
-end
-
-function downloadFile(fileName)
-  print("Downloading..."..fileName)
-  local result = ""
-  local response = internet.request(BranchURL..fileName)
-  local isGood, err = pcall(function()
-    local file, err = io.open(fileName, "w")
-    if file == nil then error(err) end
-    for chunk in response do
-      file:write(chunk)
-    end
-    file:close()
-  end)
-  if not isGood then
-    io.stderr:write("Unable to Download\n")
-    io.stderr:write(err)
-    forceExit(false)
-  end
-end
-
-onlineCheck()
-createInstallDirectory()
-checkForExistingInstall()
-downloadNeededFiles()
-createSystemShortcut()
-print("Launcher Changes:")
-for i,v in ipairs(ReleaseVersions.launcher.note) do print("  "..v) end
-print([[
-Installation complete!
-Please use the 'ags' system command to run the launcher.
-]])
-]]--
