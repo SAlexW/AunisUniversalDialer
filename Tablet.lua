0ed57841026e6066e1598794867d8c41d60eee8c
SAlexW <38391381+SAlexW@users.noreply.github.com>
2020-12-31 20:09:07 +1000

    Update Tablet.lua

--- Tablet.lua
+++ Tablet.lua
@@ -1093,7 +1093,6 @@ if (tun and recev == tunnel.address) then
 	num = num+1
    end
   ::mainbookend::
-  mainbook:close()
   end
   local mainaddtimed = mainadd[1]
    if (table.concat(mainadd[1]) == "#") then
