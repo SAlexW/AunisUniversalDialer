0df225c581a392f8deedfabb98e68565fd81cc2e
SAlexW <38391381+SAlexW@users.noreply.github.com>
2022-07-17 15:26:04 +1000

    Update Installer.lua

--- Installer.lua
+++ Installer.lua
@@ -2,68 +2,71 @@
 Creator: Ragog/SALexW
 Installer ver: v1.0.0.0
 ]]--
-
-
+ 
+ 
 local comp = require("component")
 local serial = require("serialization")
 local fsys = require("filesystem")
 local shell = require("shell")
 local term = require("term")
-local gpu = component.gpu
+local gpu = comp.gpu
 local wwweb = nil
-local webconnect = component.isAvailable("internet")
-if webconnect then wwweb = require("internet") else io.stderr:write("No internet connection. Please install an\nInternet Card\n") os.exit(false) end
+local webconnect = comp.isAvailable("internet")
+if webconnect then wwweb = require("internet") else io.stderr:write("No internet connection. Please install an Internet Card") os.exit(false) end
 local type = false
-
+ 
 function dircreate()
- if not fsys.isDirectory("/arud") then
-  local sccs, msg = fsys.makeDirectory("/arud")
+ if not fsys.isDirectory("/aurd") then
+  local sccs, msg = fsys.makeDirectory("/aurd")
   if sccs == nil then
    io.stderr:write("ERROR: Failed to create directory")
    os.exit(false)
   end
  end
+ selecttype()
 end
-
+ 
 function selecttype()
  print("Please select the device type for the program you are installing:")
  print("1 - Tablet, 2 - Computer/Server, Other - Cancel installation")
  local input = io.read()
- if (input == "1" or choose == "2") then
+ if (input == "1" or input == "2") then
   if input == "1" then type = true else type = false end
+  download()
  else
  io.stderr:write("Instalation canceled")
  os.exit(false)
  end
 end
-
+ 
 function download()
  print("Downloading files...")
  if type then
- shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalDialer/main/Tablet.lua /arud/AURD.lua -fQ")
- shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalDialer/main/MWAS.ff /arud/MWAS.ff -fQ")
- shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalDialer/main/MWDS.ff /arud/MWDS.ff -fQ")
- shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalDialer/main/MWGS.ff /arud/MWGS.ff -fQ")
- shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalDialer/main/MWG.ff /arud/MWG.ff -fQ")
- shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalDialer/main/PGAS.ff /arud/PGAS.ff -fQ")
- shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalDialer/main/PGDS.ff /arud/PGDS.ff -fQ")
- shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalDialer/main/PGGS.ff /arud/PGGS.ff -fQ")
- shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalDialer/main/PGG.ff /arud/PGG.ff -fQ")
- shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalDialer/main/UNG.ff /arud/UNG.ff -fQ")
- shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalDialer/main/GI.ff /arud/GI.ff -fQ")
+ shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalRemoteDialer/main/Tablet.lua /aurd/AURD.lua -fQ")
+ shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalRemoteDialer/main/MWAS.ff /aurd/MWAS.ff -fQ")
+ shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalRemoteDialer/main/MWDS.ff /aurd/MWDS.ff -fQ")
+ shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalRemoteDialer/main/MWGS.ff /aurd/MWGS.ff -fQ")
+ shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalRemoteDialer/main/MWG.ff /aurd/MWG.ff -fQ")
+ shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalRemoteDialer/main/PGAS.ff /arud/PGAS.ff -fQ")
+ shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalRemoteDialer/main/PGDS.ff /arud/PGDS.ff -fQ")
+ shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalRemoteDialer/main/PGGS.ff /arud/PGGS.ff -fQ")
+ shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalRemoteDialer/main/PGG.ff /arud/PGG.ff -fQ")
+ shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalRemoteDialer/main/UNG.ff /aurd/UNG.ff -fQ")
+ shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalRemoteDialer/main/GI.ff /aurd/GI.ff -fQ")
  else
- shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalDialer/main/PC.lua /arud/AURD.lua -fQ") 
+ shell.execute("wget https://raw.githubusercontent.com/SAlexW/AunisUniversalRemoteDialer/main/PC.lua /aurd/AURD.lua -fQ") 
  end
 local autorunFile = [[
 local shell = require("shell")
-
 do
-shell.setWorkingDirectory("/arud/")
+shell.setWorkingDirectory("/aurd/")
 shell.execute("AURD.lua")
 end
 ]]
 local file = io.open("/autorun.lua", "w")
 file:write(autorunFile)
 file:close()
-print("Download completed. Please, restart your PC/Tablet. Programm will run automatically")
+print("Download completed.\n Please, restart your PC/Tablet.\n Programm will run automatically")
 end
+ 
+dircreate()
